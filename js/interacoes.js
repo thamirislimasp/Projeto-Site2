cfacf3a37e6f16b3b36d466cf5f04e3eabb876a5
thamirislimasp <thamirislimasp@gmail.com>
2024-02-21 22:14:08 -0300

    Atualização

---
+++ js/interacoes.js
@@ -0,0 +1,11 @@
+// Hamburger
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    } 
+    // Se eu clicar (abre)
+    // caso contrario (fecha)
+}
